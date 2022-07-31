5192102b8c7f58661bc76b6a40cfd06ea718de63
Mateusz Matlinski <mateusz@matlinski.com>
2022-07-31 14:31:56 +0200

    pug test2

--- mod.js
+++ mod.js
@@ -1,9 +1,9 @@
 const express = require('express')
-const pug = require('pug');
+const path = require('path');
 const app = express()
 const port = 3000
 app.set('view engine', 'pug');
-
+app.set('views', path.join(__dirname, '/views'));
 app.get('/', (req, res) => {
   res.render('index', {
     title: 'Hey',
