41fe311495f1115fe95ae2b0b3efea5e65101484
Mateusz Matlinski <mateusz@matlinski.com>
2022-07-31 19:53:39 +0200

    adding layout

--- mod.js
+++ mod.js
@@ -1,17 +1,23 @@
-const express = require('express')
+const express = require('express');
+const fetch = require('node-fetch');
+const cheerio = require('cheerio');
 const path = require('path');
-const app = express()
-const port = 3000
+const config = require(`${__dirname}/data/config`);
+const app = express();
+const port = 3000;
+
 app.use(
   express.static(__dirname + '/public')
 );
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, '/views'));
-app.get('/', (req, res) => {
-  res.render('index', {
-    title: 'Hey',
-    message: 'Hello there!'
-  })
+app.get('/', async (req, res) => {
+  const repoReq = await fetch('https://github.com/matlinski/cv/commit/master');
+  const repoRes = await repoReq.text();
+  const $ = cheerio.load(repoRes);
+  const commits = $(".js-diff-progressive-container").html();
+  config.banner = commits;
+  res.render('index', config)
 })
 
 app.listen(port, () => {
