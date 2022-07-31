9421864fcb5d429b27bd7b7bec3ee1ce9556a483
Mateusz Matlinski <mateusz@matlinski.com>
2022-07-31 15:17:58 +0200

    static files test

--- mod.js
+++ mod.js
@@ -2,6 +2,9 @@ const express = require('express')
 const path = require('path');
 const app = express()
 const port = 3000
+app.use(
+  express.static(__dirname + '/public')
+);
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, '/views'));
 app.get('/', (req, res) => {
