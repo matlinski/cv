7b1831dc25954b7b6ebdf7a98eba1feae60b8fa0
Mateusz Matlinski <mateusz@matlinski.com>
2022-08-01 09:28:26 +0200

    configs now generate from examples if no file

--- mod.js
+++ mod.js
@@ -1,18 +1,31 @@
 const express = require('express');
+const fs = require('fs')
 const fetch = require('node-fetch');
 const cheerio = require('cheerio');
 const path = require('path');
-const config = require(`${__dirname}/data/config`);
 const app = express();
 const port = 3000;
-
+const configFiles = [
+  'data/config.json',
+  'public/css/vars.css'
+];
+configFiles.forEach(path=>{
+  const indexOfExt = path.lastIndexOf('.');
+  if (!fs.existsSync(path)) {
+    fs.copyFile(
+      `${path.slice(0, indexOfExt)}-example${path.slice(indexOfExt)}`,
+      path,
+      (err) => { if (err) throw err; });    
+  }
+});
 app.use(
   express.static(__dirname + '/public')
 );
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, '/views'));
 app.get('/', async (req, res) => {
-  const repoReq = await fetch('https://github.com/matlinski/cv/commit/master');
+  const config = require(`${__dirname}/data/config.json`);
+  const repoReq = await fetch(`${config.repo}/cv/commit/master`);
   const repoRes = await repoReq.text();
   const $ = cheerio.load(repoRes);
   const commits = $(".js-diff-progressive-container").html();
